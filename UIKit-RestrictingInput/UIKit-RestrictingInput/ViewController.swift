75e5e54aff2fe3b95e5c9e03cba74f4ee1a2af7d
Darya Kuliashova <kuldiand@gmail.com>
2022-05-06 12:32:26 +0200

    Create test project

---
+++ UIKit-RestrictingInput/UIKit-RestrictingInput/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-RestrictingInput
+//
+//  Created by Darya Kuliashova on 06/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
